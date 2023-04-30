0a787e9132c089b523e33b8b37778225c44ecdd7
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-04-30 17:20:07 +0500

    learn module exports and named exports path module and os module in node js

--- server.js
+++ server.js
@@ -5,8 +5,30 @@
 // How NodeJS differs from Vanilla JS
 // 1) Node runs on a server - not in a browser (backend not frontend)
 // 2) The console is the terminal window
-console.log("Hello world");
+// console.log("Hello world");
 // 3) global object instead of window object
-console.log(global);
+// console.log(global);
 // 4) Has Common Core modules that we will explore
 // 5) CommonJS modules instead of ES6 modules
+
+const os = require("os");
+const path = require("path");
+const math = require("./math");
+
+console.log(math.add(2, 3));
+console.log(math.divide(2, 3));
+console.log(math.multiply(2, 3));
+console.log(math.subtract(2, 3));
+
+// console.log(os.type()); // Darwin
+// console.log(os.version()); // Darwin Kernel Version 22.4.0: Mon Mar  6 20:59:28 PST 2023; root:xnu-8796.101.5~3/RELEASE_ARM64_T6000
+// console.log(os.homedir()); // /Users/abdulxoliq
+
+// console.log(__dirname); // /Users/abdulxoliq/Desktop/nodejs
+// console.log(__filename); // /Users/abdulxoliq/Desktop/nodejs/server.js
+
+// console.log(path.dirname(__filename)); // /Users/abdulxoliq/Desktop/nodejs
+// console.log(path.basename(__filename)); // server.js
+// console.log(path.extname(__filename)); // .js
+
+// console.log(path.parse(__filename)); // { root: '/',dir: '/Users/abdulxoliq/Desktop/nodejs',base: 'server.js',ext: '.js',name: 'server'}
