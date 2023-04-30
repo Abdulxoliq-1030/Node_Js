c1094a3df49ad67d8c1c975e9d67b377446c90dd
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-04-30 16:57:06 +0500

    welcome node js

---
+++ server.js
@@ -0,0 +1,12 @@
+// You should already know:
+// HTML, CSS, and Javascript
+// Possibly experience with other libraries and frameworks
+
+// How NodeJS differs from Vanilla JS
+// 1) Node runs on a server - not in a browser (backend not frontend)
+// 2) The console is the terminal window
+console.log("Hello world");
+// 3) global object instead of window object
+console.log(global);
+// 4) Has Common Core modules that we will explore
+// 5) CommonJS modules instead of ES6 modules
