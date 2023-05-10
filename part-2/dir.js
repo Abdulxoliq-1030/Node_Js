4101b4d93a82b82abc43644e7ea9016bf65917d3
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-05-10 20:07:23 +0500

    mkdir and rmdir

--- part-2/dir.js
+++ part-2/dir.js
@@ -1,6 +1,15 @@
 const fs = require("fs");
 
-fs.mkdir("./new", (err) => {
-  if (err) throw err;
-  console.log("Directory created");
-});
+if (!fs.existsSync("./new")) {
+  fs.mkdir("./new", (err) => {
+    if (err) throw err;
+    console.log("Directory created");
+  });
+}
+
+if (fs.existsSync("./new")) {
+  fs.rmdir("./new", (err) => {
+    if (err) throw err;
+    console.log("Directory removed");
+  });
+}
