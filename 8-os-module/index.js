e821facd53518e29ff52b27de6f15b66b97f7405
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-05-15 09:19:18 +0500

    complete 8  os module

---
+++ 8-os-module/index.js
@@ -0,0 +1,10 @@
+const os = require("os");
+
+const freeMem = os.freemem();
+const userInfo = os.userInfo();
+const busyMem = os.totalmem();
+
+console.log(busyMem);
+
+console.log(`Bo'sh hotira miqdori: ${freeMem} bayt`);
+console.log(`Foydalanuvchi haqida malumot: ${userInfo.username}`);
