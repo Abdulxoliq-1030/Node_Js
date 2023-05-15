47a25e7d9e839d9d7722b19ceb06594ec8eaaab4
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-05-15 11:14:44 +0500

    complete semantic versioning

---
+++ 12-semantic-versioning/index.js
@@ -0,0 +1,10 @@
+//   "version": "1.0.0" 1=> son MAJOR, 2=> son MINOR, 3=> son PATCH
+
+// PATCH => dasturda qandaydur bug bo'lib  uni tuzatilganda shu patch versiyasiy o'zgartiriladi.
+
+// MINOR => dasturga yangi hususiyat qo'shilganda oshiriladi bu qanday hususiyatgi dasturni api sini buzmaydigan hususiyat qo'shilganda oshiriladi. Bunday o'zgarish NONE BREAKING CHANGE deyiladi, bu o'zgarsa patch ham 0 bo'ladi.
+
+// MAJOR => dasturga uni api si o'zgarishiga sabab bo'ladigan katta o'zgarish qo'shilganda uning major versiyasi o'zgaradi.Bu o'zgarsa minor bilan patch ham 0 bo'ladi chunki bu chiqgan yangi versiyada hali bug chiqmagan bo'ladi bug chiqganidan keyin u o'zgaradi.
+
+// "^5.7.7" => caret ^ // v5.x
+// "tilda5.7.7" => tilda v5.7.x
