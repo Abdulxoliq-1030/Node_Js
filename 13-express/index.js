f1029e8f113cc35f93b02cf38d9ca9d62e7180e9
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-05-16 21:15:56 +0500

    express init codes

---
+++ 13-express/index.js
@@ -0,0 +1,12 @@
+// https://virtualdars.com/api/customers
+
+// protocol => https://
+// domain => virtualdars.com
+// optional => /api
+// resource => /customers
+
+// GET => Ma'lumotni hammasini yoki bittasini olish
+// POST => Yangi ma'lumot yaratish
+// PUT => Ma'lumotni yangilash
+// PATCH => Ma'lumotni qisman yangilash
+// DELETE => Ma'lumotni o'chirish
