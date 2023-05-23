1be4580aedfadcce55b67e1949caabfca071178b
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-05-23 22:08:41 +0500

    using post request

--- 13-express/index.js
+++ 13-express/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
-
 const app = express();
+
+app.use(express.json());
 
 const books = [
   { id: 1, name: "rich dad poor dad" },
@@ -17,15 +18,24 @@ app.get("/api/books", (req, res) => {
   res.send(books);
 });
 
+app.post("/api/books", (req, res) => {
+  const book = {
+    id: books.length + 1,
+    name: req.body.name,
+  };
+  books.push(book);
+  res.status(201).send(book);
+});
+
 app.get("/api/books/:id", (req, res) => {
   const book = books.find((b) => b.id === parseInt(req.params.id));
   if (!book) res.status(404).send("Berilgan ID ga teng kitob topilmadi");
   res.send(book);
 }); // paramslarni olish uchun
 
-app.get("/api/articles/:year/:month", (req, res) => {
-  res.send(req.query);
-}); // query objectni olish uchun
+// app.get("/api/articles/:year/:month", (req, res) => {
+//   res.send(req.query);
+// }); // query objectni olish uchun
 
 app.get("/api/articles/:year/:month", (req, res) => {
   res.send(req.params);
