619ccda4734ce1b7ef167e669658f113f6352f43
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-05-23 22:01:47 +0500

    working route parameters

--- 13-express/index.js
+++ 13-express/index.js
@@ -2,21 +2,25 @@ const express = require("express");
 
 const app = express();
 
+const books = [
+  { id: 1, name: "rich dad poor dad" },
+  { id: 2, name: "good to great" },
+  { id: 3, name: "how to win friends and influence people" },
+  { id: 4, name: "the war of art" },
+];
+
 app.get("/", (req, res) => {
   res.send("Salom");
 });
 
 app.get("/api/books", (req, res) => {
-  res.send([
-    "rich dad poor dad",
-    "good to great",
-    "how to win friends and influence people",
-    "the war of art",
-  ]);
+  res.send(books);
 });
 
 app.get("/api/books/:id", (req, res) => {
-  res.send(req.params.id);
+  const book = books.find((b) => b.id === parseInt(req.params.id));
+  if (!book) res.status(404).send("Berilgan ID ga teng kitob topilmadi");
+  res.send(book);
 }); // paramslarni olish uchun
 
 app.get("/api/articles/:year/:month", (req, res) => {
