479722e6f1689825e0fed90563a850f658c2c206
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-05-28 09:59:38 +0500

    complete exexcise

---
+++ 13-express/exercise.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const Joi = require("joi");
+const app = express();
+
+app.use(express.json());
+
+const categories = [
+  { id: 1, name: "Dasturlash" },
+  { id: 2, name: "Ma'lumot omborlari" },
+  { id: 3, name: "Kompyuter tarmoqlari" },
+];
+
+app.get("/api/categories", (req, res) => {
+  res.send(categories);
+});
+
+app.post("/api/categories", (req, res) => {
+  const { error } = validateCategory(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  const category = {
+    id: categories.length + 1,
+    name: req.body.name,
+  };
+  categories.push(category);
+  res.status(201).send(category);
+});
+
+app.get("/api/categories/:id", (req, res) => {
+  const category = categories.find((c) => c.id === parseInt(req.params.id));
+  if (!category)
+    return res.status(404).send("Berilgan ID ga teng bo'lgan toifa topilmadi");
+
+  const categoryIndex = categories.indexOf(category);
+  categories.splice(categoryIndex, 1);
+  res.send(category);
+});
+
+app.put("/api/categories/:id", (req, res) => {
+  const category = categories.find((c) => c.id === parseInt(req.params.id));
+  if (!category) res.status(404).send("Not Found!!!");
+  const { error } = validateCategory(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  category.name = req.body.name;
+  res.send(category);
+});
+
+app.delete("/api/categories/:id", (req, res) => {
+  const category = categories.find((c) => c.id === parseInt(req.params.id));
+
+  if (!category) {
+    return res.status(404).send("Not Found!!!");
+  }
+
+  const categoryIndex = categories.indexOf(category);
+  categories.splice(categoryIndex, 1);
+  res.send(category);
+});
+
+function validateCategory(category) {
+  const schema = {
+    name: Joi.string().required().min(3),
+  };
+
+  return Joi.validate(category, schema);
+}
+
+const port = process.env.PORT || 6000;
+
+app.listen(port, () => {
+  console.log(`${port} portni eshitishni boshladim...`);
+});
