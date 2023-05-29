65a17d9e8ffa620f4af460f20609271f6d1e5d32
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-05-29 11:26:43 +0500

    mongoose schema

--- 14-mongodb/index.js
+++ 14-mongodb/index.js
@@ -8,3 +8,13 @@ mongoose
   .catch((err) => {
     console.error("MongoDBga ulanish vaqtida hato ro'y berdi...", err);
   });
+
+const bookSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+
+// String,Number,Date,Buffer,Boolean,ObjectId,Array
