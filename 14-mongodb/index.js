80754dd3735b5244278b240807c5cac21823f871
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-06-02 05:54:45 +0500

    first exercise

--- 14-mongodb/index.js
+++ 14-mongodb/index.js
@@ -32,8 +32,11 @@ async function createBook() {
 }
 
 async function getBooks() {
+  const pageNumber = 3;
+  const pageSize = 10;
   const books = await Book.find({ author: "Abdulxoliq Odiljonov" })
-    .limit(2)
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
 
@@ -53,3 +56,48 @@ getBooks();
 // $lte (less than or equal)
 // $in
 // $nin (not in)
+
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => {
+//         console.log('MongoDBga ulanish hosil qilindi...');
+//     })
+//     .catch((err) => {
+//         console.error('MongoDBga ulanish vaqtida xato ro\'y berdi...', err);
+//     });
+
+// const SizeSchema = new mongoose.Schema({
+//     h: Number,
+//     w: Number,
+//     uom: String
+// })
+
+// const inventorySchema = new mongoose.Schema({
+//     item: String,
+//     qty: Number,
+//     size: SizeSchema,
+//     status: String
+// }, { collection: 'inventory' });
+
+// const Inventory = mongoose.model('Inventory', inventorySchema);
+
+// async function getInventoryItems1() {
+//     return await Inventory
+//         .find({ status: 'A' })
+//         .sort({ item: 1 })
+//         .select({ item: 1, qty: 1, _id: 0 })
+// }
+
+// async function getInventoryItems2() {
+//     return await Inventory
+//         .find()
+//         .or([{ qty: { $lte: 50 } }, { item: /.*l.*/i }])
+//         .sort({ qty: -1 })
+// }
+
+// async function run() {
+//     const items = await getInventoryItems2();
+//     console.log(items);
+// }
+
+// run();
